e46ab2605ab0bec98123d0a07eae7e4573b93035
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2021-10-08 21:30:28 +0500

    update

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -11,28 +11,28 @@ const router = require('./router');
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 app.use('/', router);
 
 // Bot
-const { Telegraf } = require("telegraf");
-const { commands } = require("../bot/commands");
-const { hears } = require("../bot/hears");
-const { listeners } = require("../bot/listeners");
-const bot = new Telegraf(config.botToken);
+// const { Telegraf } = require("telegraf");
+// const { commands } = require("../bot/commands");
+// const { hears } = require("../bot/hears");
+// const { listeners } = require("../bot/listeners");
+// const bot = new Telegraf(config.botToken);
 
 // bot.telegram.setWebhook(`${process.env.WEBSITE}/bot${config.botToken}`);
 // app.use(bot.webhookCallback(`/bot${config.botToken}`));
 
-commands(bot);
-hears(bot);
-listeners(bot);
+// commands(bot);
+// hears(bot);
+// listeners(bot);
 
-bot.catch((err, ctx) => {
-    console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
-    ctx.reply("Xatolik yuz berdi");
-});
+// bot.catch((err, ctx) => {
+//     console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
+//     ctx.reply("Xatolik yuz berdi");
+// });
 
 module.exports = app;
