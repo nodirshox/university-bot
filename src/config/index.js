901a5466b2028a4c16e274420f39ed10d0eb399a
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2021-10-08 15:06:06 +0500

    update

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 const config = {
 	mongoURL: getConf("MONGO_URL", "mongodb://localhost:27017/bot"),
-	botToken: getConf("BOT_TOKEN", "default")
+	botToken: getConf("BOT_TOKEN", "default"),
+	httpPort: getConf("HTTP_PORT", 3000)
 };
 
 function getConf(name, def = "") {
