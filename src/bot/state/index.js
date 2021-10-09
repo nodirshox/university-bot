91b3b04ac5de2e02584d7a7412d6e40214036dc6
Nodirbek Ergashev <mr.nodirbek77@gmail.com>
2021-10-09 20:48:14 +0500

    change page limit and order

--- src/bot/state/index.js
+++ src/bot/state/index.js
@@ -1,7 +1,7 @@
 exports.GET_INFO = "GET_INFO";
 exports.NEXT_PAGE = "NEXT_PAGE";
-exports.PAGE_LIMIT = 3;
-exports.ORDER_LIST = 1;
+exports.PAGE_LIMIT = 6;
+exports.ORDER_LIST = -1;
 exports.BTN_UNIVERSITY = "🎓 Barcha universitetlar";
 exports.BTN_CHANNEL = "🔰 Kanal";
 exports.BTN_GROUP = "🗯 Guruh";
